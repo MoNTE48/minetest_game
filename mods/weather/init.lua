e603e1d674765043f8d0561cf76ed1d334a26b8e
sfan5 <sfan5@live.de>
2024-04-10 18:24:00 +0200

    Add API to weather mod
    Co-authored-by: Till Affeldt <t.affeldt@tu-braunschweig.de>

--- mods/weather/init.lua
+++ mods/weather/init.lua
@@ -1,3 +1,6 @@
+-- Always load the API
+dofile(minetest.get_modpath(minetest.get_current_modname()) .. "/api.lua")
+
 -- Disable by mapgen or setting
 
 local mg_name = minetest.get_mapgen_setting("mg_name")
@@ -7,10 +10,9 @@ if mg_name == "v6" or mg_name == "singlenode" or
 end
 
 
--- Parameters
+-- Default implementation for noise based cloud appearance
 
 local TSCALE = 600 -- Time scale of noise variation in seconds
-local CYCLE = 8 -- Time period of cyclic clouds update in seconds
 
 local np_density = {
 	offset = 0.5,
@@ -52,18 +54,10 @@ local np_speedz = {
 	lacunarity = 2,
 }
 
--- End parameters
-
-
--- Initialise noise objects to nil
-
 local nobj_density = nil
 local nobj_thickness = nil
 local nobj_speedx = nil
 local nobj_speedz = nil
-
-
--- Update clouds function
 
 local function rangelim(value, lower, upper)
 	return math.min(math.max(value, lower), upper)
@@ -81,7 +75,7 @@ do
 	end
 end
 
-local function update_clouds()
+function weather.get(player)
 	-- Adjusted time in seconds
 	local time = math.floor(minetest.get_gametime() - t_offset)
 
@@ -95,40 +89,25 @@ local function update_clouds()
 	local n_speedx = nobj_speedx:get_2d({x = time, y = 0}) -- -1 to 1
 	local n_speedz = nobj_speedz:get_2d({x = time, y = 0}) -- -1 to 1
 
-	for _, player in ipairs(minetest.get_connected_players()) do
-		-- Fallback to mid-value 50 for very old worlds
-		local humid = minetest.get_humidity(player:get_pos()) or 50
-		-- Default and classic density value is 0.4, make this happen
-		-- at humidity midvalue 50 when n_density is at midvalue 0.5.
-		-- density_max = 0.25 at humid = 0.
-		-- density_max = 0.8 at humid = 50.
-		-- density_max = 1.35 at humid = 100.
-		local density_max = 0.8 + ((humid - 50) / 50) * 0.55
-		-- Range limit density_max to always have occasional
-		-- small scattered clouds at extreme low humidity.
-		local density = rangelim(density_max, 0.2, 1.0) * n_density
-		player:set_clouds({
+	-- Fallback to mid-value 50 for very old worlds
+	local humid = minetest.get_humidity(player:get_pos()) or 50
+	-- Default and classic density value is 0.4, make this happen
+	-- at humidity midvalue 50 when n_density is at midvalue 0.5.
+	-- density_max = 0.25 at humid = 0.
+	-- density_max = 0.8 at humid = 50.
+	-- density_max = 1.35 at humid = 100.
+	local density_max = 0.8 + ((humid - 50) / 50) * 0.55
+	-- Range limit density_max to always have occasional
+	-- small scattered clouds at extreme low humidity.
+	local density = rangelim(density_max, 0.2, 1.0) * n_density
+
+	return {
+		clouds = {
 			density = density,
 			thickness = math.max(math.floor(
 				rangelim(32 * humid / 100, 8, 32) * n_thickness
 				), 2),
 			speed = {x = n_speedx * 4, z = n_speedz * 4},
-		})
-	end
+		},
+	}
 end
-
-
-local function cyclic_update()
-	update_clouds()
-	minetest.after(CYCLE, cyclic_update)
-end
-
-
-minetest.after(0, cyclic_update)
-
-
--- Update on player join to instantly alter clouds from the default
-
-minetest.register_on_joinplayer(function(player)
-	update_clouds()
-end)
